d121a7a20c7a6503bc1682e5f88a43fe1008a9e4
Yaksh Tyagi <yakshtyagiyt@gmail.com>
2021-12-08 10:25:17 +0530

    07-12-21

---
+++ FrontendEcom/Models/SellerRegistration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontendEcom.Models
+{
+    public class SellerRegistration
+    {
+        public string Gst { get; set; }
+        public decimal CompanyContact { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
